20c76e666e0be35f81cb8859c78b1679166c2121
D4LGONA(samsung) <laff3y2297@gmail.com>
2023-12-30 23:54:21 +0900

    12/30

---
+++ 2023_winter/3052.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n;
+    vector<int> v;
+    for (int i = 0; i < 10; ++i)
+    {
+        cin >> n;
+        bool flag = false;
+        for (int j = 0; j < v.size(); ++j)
+        {
+            if (v[j] == n % 42)
+            {
+                flag = true;
+                break;
+            }
+        }
+        if (!flag)
+            v.push_back(n % 42);
+    }
+    cout << v.size();
+}
